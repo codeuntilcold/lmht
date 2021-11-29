0776e34ae51336c9ad6b178cf1d08414fb1e4010
codeuntilcold <quocdungngole@gmail.com>
2021-11-29 20:04:58 +0700

    merge chếc tiệc

--- controllers/skins.controller.js
+++ controllers/skins.controller.js
@@ -69,12 +69,11 @@ module.exports = {
 
     async postSkin(req, res) {
 
+
         async function getByChampName(connection, binds, options, champName) {
 
-            async function getByChampName(connection, binds, options, champName) {
-
-                // VULNERABLE TO SQL INJECTION
-                const sql = `
+            // VULNERABLE TO SQL INJECTION
+            const sql = `
                 SELECT
                     MA_TUONG,
                     MA_TRANG_PHUC,
@@ -84,63 +83,63 @@ module.exports = {
                     NGAY_RA_MAT
                 FROM TUONG NATURAL JOIN TRANG_PHUC
                 ${champName &&
-                    "WHERE TEN_TUONG = '" + champName.toUpperCase() + "'"
-                    }
+                "WHERE TEN_TUONG = '" + champName.toUpperCase() + "'"
+                }
                 ORDER BY MA_TRANG_PHUC DESC`
 
-                return await connection.execute(sql, binds, options);
+            return await connection.execute(sql, binds, options);
+        }
+
+
+
+        try {
+
+            const champName = req.body.champName
+            const method = req.body.clientMethod
+
+            let sql, binds, options, result;
+
+            binds = {};
+            options = {
+                outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
+                autoCommit: true,   // save changes immediately
+            };
+
+
+
+            connection = await db.connect()
+
+            if (method === 'GET') {
+
+                // VULNERABLE TO SQL INJECTION
+                // sql = `
+                //     SELECT
+                //         MA_TUONG,
+                //         MA_TRANG_PHUC,
+                //         TEN_TUONG,
+                //         CHU_DE,
+                //         BAC,
+                //         NGAY_RA_MAT
+                //     FROM TUONG NATURAL JOIN TRANG_PHUC
+                //     ${champName &&
+                //         "WHERE TEN_TUONG = '" + champName.toUpperCase() + "'"
+                //     }
+                //     ORDER BY TEN_TUONG, CHU_DE`
+                // result = await connection.execute(sql, binds, options);
+
+                result = await getByChampName(connection, binds, options, champName)
+                res.render('skins', result)
+
             }
+            else if (method === 'POST') {
 
+                // insert skin moi
+                const nameofChamp = req.body.champName
+                const skinPostfix = req.body.skinPostfix
+                const skinLevel = req.body.skinLevel || 'Không'
+                const date = req.body.date || '12-DEC-2021'
 
-
-            try {
-
-                const champName = req.body.champName
-                const method = req.body.clientMethod
-
-                let sql, binds, options, result;
-
-                binds = {};
-                options = {
-                    outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
-                    autoCommit: true,   // save changes immediately
-                };
-
-
-
-                connection = await db.connect()
-
-                if (method === 'GET') {
-
-                    // VULNERABLE TO SQL INJECTION
-                    // sql = `
-                    //     SELECT
-                    //         MA_TUONG,
-                    //         MA_TRANG_PHUC,
-                    //         TEN_TUONG,
-                    //         CHU_DE,
-                    //         BAC,
-                    //         NGAY_RA_MAT
-                    //     FROM TUONG NATURAL JOIN TRANG_PHUC
-                    //     ${champName &&
-                    //         "WHERE TEN_TUONG = '" + champName.toUpperCase() + "'"
-                    //     }
-                    //     ORDER BY TEN_TUONG, CHU_DE`
-                    // result = await connection.execute(sql, binds, options);
-
-                    result = await getByChampName(connection, binds, options, champName)
-                    res.render('skins', result)
-
-                }
-                else if (method === 'POST') {
-
-                    // insert skin moi
-                    const nameofChamp = req.body.champName
-                    const skinPostfix = req.body.skinPostfix
-                    const skinLevel = req.body.skinLevel || 'Không'
-                    const date = req.body.date || '12-DEC-2021'
-
-                    sql = `
+                sql = `
                     SELECT
                         C.MA_TUONG,
                         S.MA_TRANG_PHUC
@@ -150,71 +149,70 @@ module.exports = {
                     ORDER BY S.MA_TRANG_PHUC
                 `
 
-                    result = await connection.execute(sql)
-                    console.log(result)
+                result = await connection.execute(sql)
+                console.log(result)
 
-                    const matuong = result.rows[0][0]
-                    const matp = result.rows[result.rows.length - 1][1] || matuong
+                const matuong = result.rows[0][0]
+                const matp = result.rows[result.rows.length - 1][1] || matuong
 
-                    sql = `
+                sql = `
                     INSERT INTO TRANG_PHUC VALUES (${matuong}, ${matp + 1}, '${nameofChamp.toUpperCase()}', '${skinPostfix}', '${date}', '${skinLevel}')
                 `
 
-                    const finalresult = await connection.execute(sql, binds, options)
+                const finalresult = await connection.execute(sql, binds, options)
 
-                    console.log(finalresult)
+                console.log(finalresult)
 
-                    res.render('skins', await getByChampName(connection, binds, options, champName))
-                }
-                else if (method === 'PUT') {
+                res.render('skins', await getByChampName(connection, binds, options, champName))
+            }
+            else if (method === 'PUT') {
 
-                    const champID = req.body.champID
-                    const skinID = req.body.skinID
-                    const skinPostfix = req.body.skinPostfix
-                    const skinLevel = req.body.skinLevel
+                const champID = req.body.champID
+                const skinID = req.body.skinID
+                const skinPostfix = req.body.skinPostfix
+                const skinLevel = req.body.skinLevel
 
-                    sql = `
+                sql = `
                     UPDATE TRANG_PHUC
                     SET CHU_DE = '${skinPostfix}', BAC = '${skinLevel}'
                     WHERE MA_TUONG = ${champID} AND MA_TRANG_PHUC = ${skinID}
                 `
 
-                    console.log(await connection.execute(sql, binds, options))
-                    result = await getByChampName(connection, binds, options, champName)
-                    res.render('skins', result)
-                }
-                else if (method === 'DELETE') {
+                console.log(await connection.execute(sql, binds, options))
+                result = await getByChampName(connection, binds, options, champName)
+                res.render('skins', result)
+            }
+            else if (method === 'DELETE') {
 
-                    const champID = req.body.champID
-                    const skinID = req.body.skinID
+                const champID = req.body.champID
+                const skinID = req.body.skinID
 
-                    sql = `
+                sql = `
                     DELETE FROM TRANG_PHUC
                     WHERE MA_TUONG = ${champID} AND MA_TRANG_PHUC = ${skinID}
                 `
-                    console.log(sql)
+                console.log(sql)
 
-                    result = await connection.execute(sql, binds, options)
-                    // console.log(result)
-                    // res.render('skins')
-                    res.render('skins', await getByChampName(connection, binds, options, champName))
-                }
+                result = await connection.execute(sql, binds, options)
+                // console.log(result)
+                // res.render('skins')
+                res.render('skins', await getByChampName(connection, binds, options, champName))
+            }
 
-            } catch (err) {
+        } catch (err) {
 
-                console.error(err);
-                res.render('skins')
+            console.error(err);
+            res.render('skins')
 
-            } finally {
-                if (connection) {
-                    try {
-                        await connection.close();
-                    } catch (err) {
-                        console.error(err);
-                    }
+        } finally {
+            if (connection) {
+                try {
+                    await connection.close();
+                } catch (err) {
+                    console.error(err);
                 }
             }
         }
-
     }
+
 }
